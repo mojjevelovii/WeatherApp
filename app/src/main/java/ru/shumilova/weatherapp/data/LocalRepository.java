3562677118b2b03a24c1b0c7b56faf75023c9281
Veronika Shumilova <mojjevelovii@gmail.com>
2020-05-14 20:20:29 +0300

    Добавлено корректное восстановление предыдущего города после неверно введенного

--- app/src/main/java/ru/shumilova/weatherapp/data/LocalRepository.java
+++ app/src/main/java/ru/shumilova/weatherapp/data/LocalRepository.java
@@ -9,12 +9,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import ru.shumilova.weatherapp.main_screen.MainFragment;
+
 public class LocalRepository {
     private SharedPreferences sharedPreferences;
     private Set<String> citySet = new HashSet<>();
+    private static final String SELECTED_CITY = "SELECTED_CITY";
     private static final String LOCAL_REPOSITORY = "LOCAL_REPOSITORY";
     private static final String CITIES = "CITIES";
     private static final String DARK_THEME = "DARK_THEME";
+
 
     public LocalRepository(Context context) {
         this.sharedPreferences = context.getSharedPreferences(LOCAL_REPOSITORY, Context.MODE_PRIVATE);
@@ -51,5 +55,14 @@ public class LocalRepository {
         SharedPreferences.Editor spEditor = sharedPreferences.edit();
         spEditor.putBoolean(DARK_THEME, condition);
         spEditor.apply();
+    }
+
+    public void saveSelectedCity(String cityName) {
+        SharedPreferences.Editor spEditor = sharedPreferences.edit();
+        spEditor.putString(SELECTED_CITY, cityName).apply();
+    }
+
+    public String getSelectedCity() {
+        return sharedPreferences.getString(SELECTED_CITY, "Moscow,RU");
     }
 }
