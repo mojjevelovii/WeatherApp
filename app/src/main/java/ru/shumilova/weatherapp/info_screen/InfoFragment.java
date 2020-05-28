80d7a174f609eabacf03707f59dff357d5cc8087
Veronika Shumilova <mojjevelovii@gmail.com>
2020-05-28 22:20:54 +0300

    Реализовала загрузку изображения через Glide

--- app/src/main/java/ru/shumilova/weatherapp/info_screen/InfoFragment.java
+++ app/src/main/java/ru/shumilova/weatherapp/info_screen/InfoFragment.java
@@ -2,11 +2,16 @@ package ru.shumilova.weatherapp.info_screen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
 
 import ru.shumilova.weatherapp.R;
 
@@ -32,5 +37,14 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AppCompatImageView foto = view.findViewById(R.id.iv_foto);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1567853843913-006267e1b4b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80")
+                .into(foto);
     }
 }
