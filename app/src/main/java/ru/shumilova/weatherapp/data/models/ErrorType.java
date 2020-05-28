321afc9d8eeb030cb93666338db3e2900d95cf4e
Veronika Shumilova <mojjevelovii@gmail.com>
2020-05-28 21:15:37 +0300

    Перевела WeatherRepository на Retrofit

--- app/src/main/java/ru/shumilova/weatherapp/data/models/ErrorType.java
+++ app/src/main/java/ru/shumilova/weatherapp/data/models/ErrorType.java
@@ -3,7 +3,9 @@ package ru.shumilova.weatherapp.data.models;
 import ru.shumilova.weatherapp.R;
 
 public enum ErrorType {
-    CITY_NOT_FOUND(R.string.error_msg_city_not_found), CONNECTION_ERROR(R.string.error_msg_no_connection);
+    CITY_NOT_FOUND(R.string.error_msg_city_not_found),
+    CONNECTION_ERROR(R.string.error_msg_no_connection),
+    UNKNOWN_ERROR (R.string.error_msg_unknown);
 
     private int errorMsg;
 
